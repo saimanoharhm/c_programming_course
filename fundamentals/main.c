e39fddc5d870336dbb4bea8a3eccab2e760de3d7
ANNAM SAI MANOHAR <saimanohar987@gmail.com>
2021-05-13 22:45:26 +0530

    Create main.c

---
+++ fundamentals/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>    //This is a header file
+#include <stdlib.h>
+#include "sai.h"
+
+/*int is a data type and
+main is a function.
+*/
+
+int main()
+{
+    display();
+    int a = 10; // int means integer
+    int b = 20;
+    int c;
+    //void = empty or nothing
+    char d = 's'; // char is character
+    float e = 2.5;
+    double f = 2.5;
+    //sai
+    //_sai
+    //sai_manohar
+    //saiManohar
+    //sai855manohar
+    //saiManohar
+    c = 5;
+    printf("%d\n"c);
+    return 0;
+}
