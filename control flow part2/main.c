ed101e3dea5d212c302ada8a57aaca6b718b42e5
ANNAM SAI MANOHAR <saimanohar987@gmail.com>
2021-05-17 23:49:58 +0530

    Update main.c

--- control flow part2/main.c
+++ control flow part2/main.c
@@ -9,7 +9,7 @@ int main()
     scanf("%d %d",&a,&b);
     (a>b) ? printf("a is greater than b\n") : printf("a is less than b\n");
     */
-    //switch statement
+    //Switch statement
     int day;
     printf("Enter a day number: ");
     scanf("%d",&day);
@@ -18,10 +18,34 @@ int main()
     case 1: printf("Monday");
         break;
     case 2: printf("Tuesday");
+
+    case 3: printf("Wed");
+
+    case 4: printf("Thur");
+
+    case 5: printf("Fri");
         break;
-    case 3: printf("Wednesday");
+    case 6: printf("Satur");
         break;
-    default:
+    case 7: printf("Sun");
+        break;
+    default: printf("Invalid day number.");
     }
     return 0;
 }
+/*
+int main()
+{
+    int a,b;
+    printf("Enter the values a & b: ");
+    scanf("%d %d",&a,&b);
+    if(a>b)
+    {
+        printf("a is greater than b\n");
+    }
+    else{
+        printf("a is less than b\n");
+    }
+    return 0;
+}
+*/
