5cabc1970de7d9283e62a6cfe5ece0f6a73dff65
ANNAM SAI MANOHAR <saimanohar987@gmail.com>
2021-05-17 23:48:35 +0530

    Create main.c

---
+++ control flow part3/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //while loop
+    /*int i = 1;
+    while(i<=100){
+        printf("%d\n",i);
+        i++;
+    }*/
+    // do-while loop
+    /*int a = 1;
+    do{
+        printf("%d\n",a);
+        a++;
+    }while(a<=10);
+    */
+    //for loop
+    for(int i = 1;i<=10;i++){
+        printf("%d\n",i);
+        printf("Hello World!\n");
+    }
+    return 0;
+}
